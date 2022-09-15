3d0f76db5e7c47c6587f5de78eb6861de1d9f881
WaskeSoo <krzysiu.s127@wp.pl>
2022-09-15 18:13:23 +0200

    Dodaj pliki projektów.

---
+++ LoginAppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoginAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
